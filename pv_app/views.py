4b542cdbae9934e41751d90b98eb0a8e8b5d8786
ubongpr20 <ubongpr20@gmail.com>
2024-08-15 08:40:16 +0000

    optimisation

--- pv_app/views.py
+++ pv_app/views.py
@@ -1,193 +1,118 @@
 from django.shortcuts import render
-from io import StringIO
 from django.http import JsonResponse
-from geopy.geocoders import Nominatim
-
-from utils.pv import *
 from django.views.generic import FormView
 from django.urls import reverse_lazy
-from .forms import PVTrackingForm,AddressForm,PlotForm
+from geopy.geocoders import Nominatim
+
+from .forms import PVTrackingForm, AddressForm, PlotForm
+from utils.pv import interactive_map, get_timezone_from_address, plot_temperature, plot_wind_speed, plot_ghi, plot_dni, plot_relative_humidity, plot_pressure, plot_dhi, pv_tracking
 
 # Create your views here.
 def home(request):
-    context={
-        'pv_tracking':pv_tracking(),
-        'map':interactive_map().get('map'),
+    context = {
+        'pv_tracking': pv_tracking(),
+        'map': interactive_map().get('map'),
     }
-    return render(request,'index.html',context)
+    return render(request, 'index.html', context)
 
 
 def get_address_suggestions(request):
     query = request.GET.get('address', '')
-    print(query)
+    suggestions = []
+
     if query:
         geolocator = Nominatim(user_agent="abcd")
-        locations = geolocator.geocode(query, exactly_one=False, limit=100)  # Limit to 5 suggestions
-        suggestions = []
+        locations = geolocator.geocode(query, exactly_one=False, limit=100)  # Limit to 100 suggestions
+
         if locations:
-            for location in locations:
-                suggestions.append(location.address)
-        return render(request, 'options.html', {'suggestions': suggestions})
-    else:
-        return render(request, 'options.html', {'suggestions': []})
+            suggestions = [location.address for location in locations]
+
+    return render(request, 'options.html', {'suggestions': suggestions})
 
 
 def map_view(request):
-    address = 'Nigeria'  
+    address = 'Nigeria'
+    form = AddressForm(request.POST or None)
     map_html = interactive_map(address)['map']
-    form = AddressForm()
-    
-    if request.method == 'POST':
-        form = AddressForm(request.POST)
-        if form.is_valid():
-            address = form.cleaned_data['address']
-            map_html = interactive_map(address)['map']
-            location = interactive_map(address).get('location',None)
-            print('location: ',location)
-            latitude=location.latitude
-            longitude=location.longitude
-            timezone=get_timezone_from_address(address)
-            print('timezone: ',timezone)
-            context = {
-                'form': form,
-                'map': map_html,
-                'latitude':latitude,
-                'longitude':longitude,
-                'timezone':timezone,
-                'location':location,
-                
-                }
+    latitude, longitude, timezone, location = None, None, None, None
 
-            return render(request, 'map.html', context)
+    if request.method == 'POST' and form.is_valid():
+        address = form.cleaned_data['address']
+        map_data = interactive_map(address)
+        map_html = map_data['map']
+        location = map_data.get('location', None)
 
-    else:
-        form = AddressForm()
-    
-    return render(request, 'map.html', {'form': form, 'map': map_html})
+        if location:
+            latitude, longitude = location.latitude, location.longitude
+            timezone = get_timezone_from_address(address)
+
+    context = {
+        'form': form,
+        'map': map_html,
+        'latitude': latitude,
+        'longitude': longitude,
+        'timezone': timezone,
+        'location': location,
+    }
+    return render(request, 'map.html', context)
+
 
 class PVTrackingView(FormView):
     template_name = 'forms.html'
     form_class = PVTrackingForm
-    success_url = '/'
+    success_url = reverse_lazy('home')
+
     def get(self, request, *args, **kwargs):
-        # Check for URL parameters
         visualizer = request.GET.get('visualizer', None)
         request.session['visualizer'] = visualizer
         return super().get(request, *args, **kwargs)
-    
 
     def form_valid(self, form):
-        # Get cleaned data from the form
-        tz = form.cleaned_data['tz']
-        from_ = form.cleaned_data['from_date']
-        to_ = form.cleaned_data['to_date']
-        lat = form.cleaned_data['lat']
-        lon = form.cleaned_data['lon']
-        freq = form.cleaned_data['freq']
-        max_angle = form.cleaned_data['max_angle']
-        axis_tilt = form.cleaned_data['axis_tilt']
-        axis_azimuth = form.cleaned_data['axis_azimuth']
-        plot_color = form.cleaned_data['plot_color']
-        plot_type = form.cleaned_data['plot_type']
-
-        # Call the pv_tracking function
-        visualizer=self.request.session.get('visualizer')
-        graph_title=''
-        result=''   
-        if visualizer=='temp':
-            result = plot_temperature(
-            tz=tz,
-            color=plot_color,
-            plot_type=plot_type,
-            # from_=from_,
-            # to_=to_,
-            lat=lat,
-            lon=lon,
-            )
-            graph_title='Temperature Variation Over Time'
-        elif visualizer=='wind':
-            result=plot_wind_speed(
-            tz=tz,
-            color=plot_color,
-            plot_type=plot_type,
-            # from_=from_,
-            # to_=to_,
-            lat=lat,
-            lon=lon,
-            )
-            graph_title='Wind Speed Variation Over Time'
-        elif visualizer=='ghi':
-            result=plot_ghi(
-            tz=tz,
-            color=plot_color,
-            plot_type=plot_type,
-            
-            lat=lat,
-            lon=lon,
-            )
-            graph_title='GHI Variation Over Time'
-        elif visualizer=='dni':
-            result=plot_dni(
-            tz=tz,
-            color=plot_color,
-            plot_type=plot_type,
-            
-            lat=lat,
-            lon=lon,
-            )
-            graph_title='DNI Variation Over Time'
-        elif visualizer=='humidity':
-            result=plot_relative_humidity(
-            tz=tz,
-            color=plot_color,
-            plot_type=plot_type,
-            
-            lat=lat,
-            lon=lon,
-            )
-            graph_title='Humidity Variation Over Time'
-        elif visualizer=='pressure':
-            result=plot_pressure(
-            tz=tz,
-            color=plot_color,
-            plot_type=plot_type,
-            
-            lat=lat,
-            lon=lon,
-            )
-            graph_title='Pressure Variation Over Time'
-        elif visualizer=='dhi':
-            result=plot_dhi(
-            tz=tz,
-            color=plot_color,
-            plot_type=plot_type,
-            
-            lat=lat,
-            lon=lon,
-            )
-            graph_title='DHI Variation Over Time'
-
-        elif visualizer=='true_tracker':
-
-            result = pv_tracking(
-                tz=tz,
-                from_=from_,
-                to_=to_,
-                lat=lat,
-                color=plot_color,
-                plot_type=plot_type,
-                lon=lon,
-                freq=f'{freq}min',
-                max_angle=max_angle,
-                axis_tilt=axis_tilt,
-                axis_azimuth=axis_azimuth
-            )
-            graph_title='True Tracking Angle'
-
-
-
-
-        context = self.get_context_data(result=result,graph_title=graph_title)
+        cleaned_data = form.cleaned_data
+        visualizer = self.request.session.get('visualizer')
+        result, graph_title = self.get_plot_result(visualizer, cleaned_data)
+        context = self.get_context_data(result=result, graph_title=graph_title)
         return self.render_to_response(context)
 
+    def get_plot_result(self, visualizer, data):
+        """Return the appropriate plot result and title based on the visualizer type."""
+        plot_functions = {
+            'temp': (plot_temperature, 'Temperature Variation Over Time'),
+            'wind': (plot_wind_speed, 'Wind Speed Variation Over Time'),
+            'ghi': (plot_ghi, 'GHI Variation Over Time'),
+            'dni': (plot_dni, 'DNI Variation Over Time'),
+            'humidity': (plot_relative_humidity, 'Humidity Variation Over Time'),
+            'pressure': (plot_pressure, 'Pressure Variation Over Time'),
+            'dhi': (plot_dhi, 'DHI Variation Over Time'),
+            'true_tracker': (pv_tracking, 'True Tracking Angle'),
+        }
 
+        plot_func, title = plot_functions.get(visualizer, (None, ''))
+
+        if plot_func:
+            if visualizer == 'true_tracker':
+                result = plot_func(
+                    tz=data['tz'],
+                    from_=data['from_date'],
+                    to_=data['to_date'],
+                    lat=data['lat'],
+                    lon=data['lon'],
+                    freq=f"{data['freq']}min",
+                    max_angle=data['max_angle'],
+                    axis_tilt=data['axis_tilt'],
+                    axis_azimuth=data['axis_azimuth'],
+                    color=data['plot_color'],
+                    plot_type=data['plot_type'],
+                )
+            else:
+                result = plot_func(
+                    tz=data['tz'],
+                    lat=data['lat'],
+                    lon=data['lon'],
+                    color=data['plot_color'],
+                    plot_type=data['plot_type'],
+                )
+        else:
+            result = None
+
+        return result, title
